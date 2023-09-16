4805b967f34e46f46216e51e3c0c81cafc36beb9
pltnv123 <dotasymphony@gmail.com>
2023-09-16 18:13:37 +0300

    fix bug

--- mysite/chat/models.py
+++ mysite/chat/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Room(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
+    users = models.ManyToManyField(User, related_name='room_users')
 
 
 class Message(models.Model):
--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth.models import User
+import logging
 
 from .models import Room, Message
 
@@ -18,9 +20,21 @@ def rooms(request):
     return render(request, 'rooms.html', {'rooms': rooms})
 
 
+# @login_required
+# def room(request, room_name):
+#     room = Room.objects.get(slug=room_name)
+#     # Получаем первые 25 сообщений
+#     messages = Message.objects.filter(room=room)[0:25]
+#     return render(request, 'room.html', {'room': room, 'messages': messages})
+
+
 @login_required
 def room(request, room_name):
     room = Room.objects.get(slug=room_name)
-    # Получаем первые 25 сообщений
-    messages = Message.objects.filter(room=room)[0:25]
-    return render(request, 'room.html', {'room': room, 'messages': messages})
+    messages = Message.objects.filter(room=room).order_by('-date_added')[:25]
+
+    # Get the list of users in the room
+    online_users = room.users.all()
+    logging.debug(f"online_users: {online_users}")
+
+    return render(request, 'room.html', {'room': room, 'messages': messages, 'online_users': online_users})
