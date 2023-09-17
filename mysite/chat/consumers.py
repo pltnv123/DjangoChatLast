2b2398c392a0a854688d1155da0d273b84e76690
pltnv123 <dotasymphony@gmail.com>
2023-09-17 19:13:36 +0300

    added online users in room

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -14,11 +14,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
+        # Add the user to the room
+        await self.add_user_to_room()
+
         await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+        # Remove the user from the room
+        await self.remove_user_from_room()
 
     # Receive message from WebSocket
     async def receive(self, text_data):
@@ -52,9 +58,47 @@ class ChatConsumer(AsyncWebsocketConsumer):
             "room": room,
         }))
 
+        # Send user online event
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "user_online",
+                "online_users": get_online_users_in_room(self.room_group_name),
+            },
+        )
+
+    async def user_online(self, event):
+        # Обновление списка онлайн пользователей
+
+        # Получение списка онлайн пользователей из события
+        online_users = event['online_users']
+
+        # Отправка обновленного списка в клиент
+        await self.send(text_data=json.dumps({
+            'online_users': online_users
+        }))
+
     @sync_to_async
     def save_message(self, username, room, message):
         user = User.objects.get(username=username)
         room = Room.objects.get(slug=room)
 
         Message.objects.create(user=user, room=room, content=message)
+
+    @sync_to_async
+    def add_user_to_room(self):
+        user = User.objects.get(username=self.scope["user"].username)
+        room = Room.objects.get(slug=self.room_name)
+        room.users.add(user)
+
+    @sync_to_async
+    def remove_user_from_room(self):
+        user = User.objects.get(username=self.scope["user"].username)
+        room = Room.objects.get(slug=self.room_name)
+        room.users.remove(user)
+
+    @sync_to_async
+    def get_online_users_in_room(room_group_name):
+        group_info = ChannelLayerWrapper.get_channel_layer().group_channels(room_group_name)
+        # Extract online users from the group info and return the list
+        return [user.split('_')[-1] for user in group_info]
